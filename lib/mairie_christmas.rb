b52de2fd868f15c4d0480fa7b75865f0e40d1968
florence <perezflores34000@gmail.com>
2021-01-22 07:00:26 +0100

    ajout

--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -6,17 +6,17 @@ PAGE_URL = 'http://annuaire-des-mairies.com/val-d-oise.html'
 Annuaire_mairie = Nokogiri::HTML(URI.open(PAGE_URL))
 
 def recup_nom_mairie()
-    return Annuaire_mairie.xpath("//a[@class='lientxt']").map {|name| "#{name.text}: "}
+    return Annuaire_mairie.xpath("//a[@class='lientxt']").map {|name| name.text}
 end
 
 
 def recup_mail_mairie()
-    return Annuaire_mairie.xpath("//a[@class='lientxt']/@href").map {|mail| "#{mail.text}"}
+    return Annuaire_mairie.xpath("//a[@class='lientxt']/@href").map {|mail| mail.text}
 end
 
 mairie_mail = Hash[recup_nom_mairie.zip(recup_mail_mairie)]
 
-mairie_mail.each do |index| p index.to_a
+mairie_mail.each do |index| p [index].to_h
 end
 
 
